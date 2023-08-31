5e7bc21e6583e58fedf40fc0da5ec4e4f75d7847
Wilhem-H <wilhem.hafsa@outlook.fr>
2023-08-31 16:01:49 +0200

    add input selector

--- src/components/ConvDist.jsx
+++ src/components/ConvDist.jsx
@@ -1,27 +1,57 @@
 import { useState } from "react";
 import { distance } from "../utils/distance";
+import { distValue } from "../utils/data";
 
 export default function ConvDist() {
   const [result, setResult] = useState(0);
+  const [entries, setEntries] = useState("m");
+  const [output, setOutput] = useState("pied");
 
   const handleChange = (e) => {
-    const result = distance(e.target.value, e.target.id);
+    const result = distance(e.target.value, entries, output);
     setResult(Number(result.toFixed(4)));
   };
 
   return (
-    <div>
-      <div>
-        <label>valeur en mètre</label>
+    <div className="distContent">
+      <div className="distInput">
+        <label id="inputSelect">Choose a unit:</label>
+        <select
+          name="inputSelect"
+          id="inputSelect"
+          onChange={(e) => setEntries(e.target.value)}
+        >
+          <option value="">--Please choose an option--</option>
+          {distValue.map((val, index) => (
+            <option key={index} value={Object.keys(val)[0]}>
+              {Object.keys(val)[0]}
+            </option>
+          ))}
+        </select>
+
+        <label id="inputValue">valeur:</label>
         <input
           type="text"
           onChange={handleChange}
-          id="m"
+          id="inputValue"
           placeholder={result}
         />
       </div>
-      <div>
-        <label>valeur en pied</label>
+      <div className="distOutput">
+        <label id="outputSelect">Choose a unit:</label>
+        <select
+          name="outputSelect"
+          id="outputSelect"
+          onChange={(e) => setOutput(e.target.value)}
+        >
+          <option value="">--Please choose an option--</option>
+          {distValue.map((val, index) => (
+            <option key={index} value={Object.keys(val)[0]}>
+              {Object.keys(val)[0]}
+            </option>
+          ))}
+        </select>
+        <label id="outputValue">valeur:</label>
         <input
           type="text"
           id="pieds"
--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,26 +1,15 @@
+import { distValue } from "./data";
+
 export const distance = (value, unit1, unit2) => {
-  const defaultValue = [
-    { pied: 0.3048 },
-    { yard: 0.9144 },
-    { mille: 1609.344 },
-    { inch: 0.0254 },
-    { km: 1000 },
-    { m: 1 },
-    { dm: 0.1 },
-    { cm: 0.01 },
-    { mm: 0.001 },
-  ];
-
-  if (!unit2) {
+  if (!unit2)
     return unit1 === "m" ? value * 3.2808398950131 : value / 3.2808398950131;
-  } else if (unit2) {
-    const enterUnit = defaultValue.find((obj) => unit1 in obj); // recupère l'objet qui correspond à unit1 en m
 
-    const exitUnit = defaultValue.find((obj) => unit2 in obj); // recupère l'objet qui correspond à unit2 en m
+  const enterUnit = distValue.find((obj) => unit1 in obj); // recupère l'objet qui correspond à unit1 en m
 
-    const entrieInMetre = Object.values(enterUnit)[0] * value; // recupère valeure de unit2 en mètre et multiplie par la valeure d'entrée
-    const outInMetre = Object.values(exitUnit)[0]; // recupère valeure de unit2 en mètre
+  const exitUnit = distValue.find((obj) => unit2 in obj); // recupère l'objet qui correspond à unit2 en m
 
-    return entrieInMetre / outInMetre;
-  }
+  const entrieInMetre = Object.values(enterUnit)[0] * value; // recupère valeure de unit2 en mètre et multiplie par la valeure d'entrée
+  const outInMetre = Object.values(exitUnit)[0]; // recupère valeure de unit2 en mètre
+
+  return entrieInMetre / outInMetre;
 };
