160594bcfeb78d0b80466d724d0a7164f79a48d0
Wilhem-H <wilhem.hafsa@outlook.fr>
2023-09-01 10:54:03 +0200

    update data

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-// équivalent en mètre
+// distances en mètre
 export const distValue = [
   { pied: 0.3048 },
   { yard: 0.9144 },
@@ -11,7 +11,40 @@ export const distValue = [
   { mm: 0.001 },
 ];
 
-//équivalent en pied
+//volume en pied
+export const volValue = [
+  { m3: 1000 },
+  { dm3: 1 },
+  { cm3: 0.001 },
+  { hl: 100 },
+  { dal: 10 },
+  { l: 1 },
+  { dl: 0.1 },
+  { cl: 0.01 },
+  { ml: 0.001 },
+  { inch3: 0.0164 },
+  { ft3: 28.3168 },
+  { yard3: 764.5549 },
+  { "gallon (UK)": 4.5461 },
+  { "gallon (US)": 3.7854 },
+  { "pinte (UK)": 0.5683 },
+  { "pinte liquide (US)": 0.4732 },
+  { "pinte sèche (US)": 0.5506 },
+  { "once liquide (UK)": 0.0284 },
+  { "once liquide (US)": 0.0296 },
+];
+
+// température en degrés celcius
+export const tempValue = [
+  { celsius: 1 },
+  { fahrenheit: -17.2222 },
+  { kelvin: -272.15 },
+  { reaumur: 1.25 },
+  { newton: 3.0303 },
+  { rankine: -272.5944 },
+];
+
+//poid en kilo
 export const volValue = [
   { m3: 1000 },
   { dm3: 1 },
