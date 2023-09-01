d3d4e677a18dcd7aa83577926ce5d42d472a1a78
Wilhem-H <wilhem.hafsa@outlook.fr>
2023-09-01 14:03:19 +0200

    bug fix and add temperature

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Header from "./pages/Header";
 import ConvPoids from "./components/ConvPoids";
 import ConvDist from "./components/ConvDist";
 import ConvVol from "./components/ConvVol";
+import ConvTemp from "./components/ConvTemp";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/convpoids" element={<ConvPoids />} />
           <Route path="/convdist" element={<ConvDist />} />
           <Route path="/convvol" element={<ConvVol />} />
+          <Route path="/convtemp" element={<ConvTemp />} />
         </Routes>
       </Router>
     </div>
--- src/utils/unitePoids.js
+++ src/utils/unitePoids.js
@@ -1,7 +1,12 @@
-export const unitePoids = (value, unit) => {
-  if (unit === "kilos") {
-    return value * 2.2046;
-  } else if (unit === "livres") {
-    return value / 2.2046;
-  }
+import { weightValue } from "./data";
+
+export const unitePoids = (value, unit1, unit2) => {
+  const enterUnit = weightValue.find((obj) => unit1 in obj); // recupère l'objet qui correspond à unit1 en m
+
+  const exitUnit = weightValue.find((obj) => unit2 in obj); // recupère l'objet qui correspond à unit2 en m
+
+  const entrieInLitre = Object.values(enterUnit)[0] * value; // recupère valeure de unit2 en mètre et multiplie par la valeure d'entrée
+  const outInLitre = Object.values(exitUnit)[0]; // recupère valeure de unit2 en mètre
+
+  return entrieInLitre / outInLitre;
 };
