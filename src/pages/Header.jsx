cc3723272d35ec4cec16ae6e16c39fa7e0acc79f
CLOT Amaury <amauryclt@gmail.com>
2023-09-01 09:23:21 +0200

    commit main avant deploy

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,21 +1,6 @@
 import { Link } from "react-router-dom";
 
 export default function Header() {
-<<<<<<< HEAD
-    return (
-        <header>
-            <div>
-                <Link to="/">
-                    <h1>Unit Converter</h1>
-                </Link>
-                <Link to="/convpoids">
-                    <button>Poids</button>
-                </Link>
-            </div>
-        </header>
-    );
-}
-=======
   return (
     <header>
       <div>
@@ -32,4 +17,3 @@ export default function Header() {
     </header>
   );
 }
->>>>>>> origin/main
